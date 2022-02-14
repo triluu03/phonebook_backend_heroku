8d1a913ad9f4dc15ec60075c35d6f018e51d403d
Lưu Đức Trí <triluu@Luus-MacBook-Air.local>
2022-02-14 10:21:34 +0200

    adding cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = requre('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(express.static('build'))
 
 morgan.token('body', (request, response) => JSON.stringify(request.body))
